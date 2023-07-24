53b94d5a5bf762aa50f00753f0fcb98a0c65bb88
drnoir <chrispgodber@gmail.com>
2023-07-24 18:56:51 +0100

    - builing up prototype templates for UI - README.md adding a readme file with some basic info

---
+++ README.md
@@ -0,0 +1,5 @@
+## AutoJob by Chris Godber (DrNoir) 
+* Tech Stack
+* Node, Express with Pug 
+* Google Cloud Talent Solution API
+* Specify area, tags and frequency for sending out automated job apps to all listings meeting criteria in a given radius with tags
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// Figure out how to communicate with the GoogleJobs API
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AutoJob', yourCV: null, postcode:  '', radius: 25, frequency : 100000, tags: ['Javascript, Typescript, Developer, Programming']     });
 });
 
 module.exports = router;
